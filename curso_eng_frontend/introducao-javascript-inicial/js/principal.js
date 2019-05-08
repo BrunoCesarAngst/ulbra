93da42c19200d1dd6a0c6236c2fa7a20f6a6d025
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-05-08 19:21:37 -0300

    atualizando

--- curso_eng_frontend/introducao-javascript-inicial/js/principal.js
+++ curso_eng_frontend/introducao-javascript-inicial/js/principal.js
@@ -1,29 +1,38 @@
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhValido = true;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Altura inválida!"
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValido = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!"
+        paciente.classList.add("paciente-invalido")
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValido = false;
+        tdImc.textContent = "Altura inválida!"
+        paciente.classList.add("paciente-invalido")
+    }
+
+    if (pesoEhValido && alturaEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhValido = false;
-    tdImc.textContent = "Peso inválida!"
-}
 
-if (pesoEhValido && alturaEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
