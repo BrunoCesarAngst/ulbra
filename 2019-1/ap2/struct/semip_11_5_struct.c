93db0e49aecce7a1ade3445885b239d79660c18c
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-05-18 23:49:28 -0300

    atualizando

--- 2019-1/ap2/struct/semip_11_5_struct.c
+++ 2019-1/ap2/struct/semip_11_5_struct.c
@@ -10,21 +10,62 @@
 
 #include <stdio.h>
 #include <string.h>
+#define v 3
 
-struct cadastro_automoveis {
-    char placa [8],
-         nome [15],
-         marca [15];
-    int ano,
+struct car_registration{
+    char plate [8],
+         name [15],
+         mark [15];
+    int year,
         km;
 };
 
-int main(int argc, char *argv[])
+int main()
 {
-    do {
-        printf("Cadastro de Automóveis\n");
-        printf("Digite a placa do automóvel: ");
-        sc
-    } while (includ == s);
+    struct car_registration car[v];
+    char add_in, consult[8];
+    int i, counter = 0;
+
+    for (i = 0; i < v; ++i) {
+        do {
+            printf("\n\t Cadastro de Automóveis \n");
+            printf("Digite a placa do automóvel: ");
+            gets(car[i].plate);
+            printf("Nome do automóvel: ");
+            gets(car[i].name);
+            printf("Marca do automóvel: ");
+            gets(car[i].mark);
+            printf("Ano do automóvel: ");
+            scanf("%d", &car[i].year);
+            printf("Km do automóvel: ");
+            scanf("%d", &car[i].km);
+            __fpurge(stdin);
+            do {
+                printf("\n Deseja cadastrar outro automóvel(S ou N)? ");
+                scanf("%c", &add_in);
+                __fpurge(stdin);
+                if ((add_in != 's' || add_in != 'S') ||
+                        (add_in != 'n'|| add_in != 'N'))  {
+                    printf("\n Digite S ou N!!!");
+                }
+            } while ((add_in != 's' || add_in != 'S') ||
+                     (add_in != 'n'|| add_in != 'N'));
+            counter++;
+        } while (add_in == 's' || add_in == 'S');
+        if (add_in == 'n' || add_in == 'N') {
+            break;
+        }
+    }
+
+    if (add_in == 'n' || add_in == 'N') {
+        printf("\n\t Pesquisa por placa \n");
+        printf("Placas cadastradas: ");
+        for (i = 0; i < counter; ++i) {
+            printf("%s", car[i].plate);
+        }
+        printf("\n Qual placa desejas consultar: ");
+        gets(consult);
+    }
+
     return 0;
 }
