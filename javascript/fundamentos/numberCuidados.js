a2b0123de0ab7a33cf6843858c937412442d80c2
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-06-17 14:59:22 -0300

    atualizando

---
+++ javascript/fundamentos/numberCuidados.js
@@ -0,0 +1,11 @@
+console.log(7 / 0.000000001) //70000....
+console.log(7 / 0) //Infinity
+console.log('10' / 2) //5
+console.log('3' + 2) //32 string ganha + é um concatenador
+console.log('3' - 2) //5 string perde - não é um concatenador
+console.log('Show' * 2) //NaN
+console.log(0.1 + 0.7) //inconsistência 0.7999999999...
+// console.log(10.toString()) 
+console.log((10.345).toString(2)) //1010.0101100001010001111...
+console.log((10.345).toFixed(2)) //10.45
+console.log(('A').toString(2)) //A
