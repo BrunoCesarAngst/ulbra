a07700d5b3000e5e428d14ccd71f2ad21c5c4dba
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-08-10 14:20:43 -0300

    Desenvolvendo o app mobile

---
+++ 2019-2/OmniStack/tindev/src/pages/Login.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from "react-native";
+
+import api from '../services/api';
+
+import logo from '../assets/logo.png';
+
+export default function Login({ navigation }) {
+    const [user, setUser] = useState('');
+
+    async function handleLogin() {
+        const response = await api.post('/devs, { username: user }');
+
+        const { _id } = response.data;
+
+        navigation.navigate('Main', { _id });
+    }
+
+    return (
+        <KeyboardAvoidingView
+            behavior="padding"
+            enabled={Platform.OS === "ios"}
+            style={ styles.container }
+        >
+            <Image source={logo} />
+
+            <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuário do GitHub"
+            placeholderTextColor="#999"
+            style={styles.input}
+            value={user}
+            onChangeText={setUser}
+            />
+
+            <TouchableOpacity onPress={handleLogin} style={styles.button}>
+                <Text style={styles.buttonText}>Enviar</Text>
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30
+    },
+
+    input: {
+        height: 46,
+        alignSelf: 'center',
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 90,
+    },
+
+    button: {
+        height: 46,
+        alignSelf: 'center',
+        backgroundColor: '#DF4723',
+        borderRadius: 4,
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 145,
+    },
+
+    buttonText: {
+        color: "#FFF",
+        fontWeight: 'bold',
+        fontSize: 17,
+    },
+});
