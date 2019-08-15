120aa87b5db4be4d47d0112c06354690d30ce504
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-08-15 15:34:44 -0300

    Backend da aplicação tindev

--- 2019-2/OmniStack/backend/src/routes.js
+++ 2019-2/OmniStack/backend/src/routes.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const DevController = require('./controllers/DevController');
-const LikeController = require('./controllers/LikeController');
-const DisLikeController = require('./controllers/DisLikeController');
-
+const express = require("express");
+const DevController = require("./controllers/DevController");
+const LikeController = require("./controllers/LikeController");
+const DisLikeController = require("./controllers/DisLikeController");
 
 const routes = express.Router();
 
+routes.get("/devs", DevController.index);
+routes.post("/devs", DevController.store);
 
-routes.get('/devs', DevController.index);
-routes.post('/devs', DevController.store);
-
-routes.post('/devs/:devId/likes', LikeController.store);
-routes.post('/devs/:devId/dislikes', DisLikeController.store);
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DisLikeController.store);
 
 module.exports = routes;
--- 2019-2/OmniStack/backend/src/server.js
+++ 2019-2/OmniStack/backend/src/server.js
@@ -1,16 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const routes = require('./routes');
+const cors = require("cors");
 
-const server = express();
+const routes = require("./routes");
 
-mongoose.connect('mongodb+srv://root:4n657@cluster0-uinlh.mongodb.net/omnistack8?retryWrites=true&w=majority', {useNewUrlParser: true});
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
+const connectedUsers = {};
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+io.on("connection", socket => {
+  const { user } = socket.handshake.query;
 
-server.listen(1234);
+  connectedUsers[user] = socket.id;
+});
+
+mongoose.connect(
+  "mongodb+srv://root:4n657@cluster0-uinlh.mongodb.net/omnistack8?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
