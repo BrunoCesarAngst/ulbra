fd510ba8b9471a6b0dd27e9cd22fae71a23ee259
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-11-11 01:28:46 -0300

    DBll

---
+++ 2019-2/bd2/trigger/trabalhog2dbII/trabG2.sql
@@ -0,0 +1,10 @@
+--TODO Escolha um problema para fazer a modelagem conceitual (ex. um supermercado, uma locadora de veículos, uma clínica médica, um ambiente virtual de aprendizagem,.....). Use sua criatividade para escolher o tema do trabalho, mas o problema deve gerar pelo menos 8 tabelas.
+
+-- O trabalho deve ter:
+-- TODO 1. Descrição do problema
+-- TODO 2. Diagrama ER.
+-- TODO 3. Definição de, no mínimo, três regras de negócio que serão implementadas com triggers.
+-- TODO 4. Os triggers necessários para a implementação das regras de negócio
+-- TODO 5. Testes das triggers e resultados da execução.
+-- TODO 6. 10 consultas importantes para a gestão do negócio e as sintaxes SQL das consultas.
+-- Peso:4,0
