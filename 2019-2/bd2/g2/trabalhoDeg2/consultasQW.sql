d211c9095ec16ae39cbcfa74d53f1bed427670ad
brunocesarangst <bruno.angst@rede.ulbra.br>
2019-11-17 16:30:12 -0300

    consulta completa de agendamento

--- 2019-2/bd2/g2/trabalhoDeg2/consultasQW.sql
+++ 2019-2/bd2/g2/trabalhoDeg2/consultasQW.sql
@@ -1,4 +1,4 @@
-select professional_name as profissional, visit_date as "Data da visita", client_name as Cliente, service_description as "Descrição do serviço"
+select professional_name as profissional, visit_date as "Data da visita", client_name as Cliente, service_description as "Descrição do serviço", phone_number as Telefone
   from 
     professional_appointments pa inner join professionals p
       on p.professional_code = pa.professional_code_fk
@@ -9,7 +9,12 @@ select professional_name as profissional, visit_date as "Data da visita", client
     inner join scheduled_services ss
       on vs.visit_schedule_code = ss.visit_schedule_code_fk
     inner join services s
-      on s.service_code = ss.service_code_fk;
+      on s.service_code = ss.service_code_fk
+    inner join customers_phones cp
+      on c.client_code = cp.client_code_fk
+    inner join phones ph
+      on ph.phone_code = cp.phone_code_fk
+  order by client_name;
 
 select professional_name as Profissional, score_value as Pontos, score_comment as FeedBack
   from professionals p inner join score s 
